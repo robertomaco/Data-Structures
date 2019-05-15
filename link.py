cacf97ddaab04137b9f1f189c746bcf0088277e2
Ryan <ry1nmca@gmail.com>
2019-05-15 18:36:21 +0100

    Initial Commit(Also LinkedList Complete)

---
+++ link.py
@@ -0,0 +1,40 @@
+class Node:
+    def __init__(self, dataval=None):
+        self.dataval = dataval
+        self.nextval = None
+        self.tailval = None
+
+class SLinkedList:
+    def __init__(self):
+        self.headval = None
+
+    def listprint(self):
+        printval = self.headval
+        while printval is not None:
+            if printval is self.headval:
+                print ("Head")
+            print (printval.dataval)
+            printval = printval.nextval
+
+    def createHead(self, value):
+        self.headval = Node(value)
+
+    def addVal(self, value):
+        head = self.headval
+        temp = head
+        if head is None:
+            self.createHead(value)
+        else:
+            while temp.nextval is not None:
+                temp = temp.nextval
+            temp.nextval = Node(value)
+    
+    def prepend(self, value):
+        head = self.headval
+        temp = head
+        if head is None:
+            self.createHead(value)
+        else:
+            prependval = Node(value)
+            prependval.nextval = self.headval
+            self.headval = prependval
---
+++ main.py
@@ -0,0 +1,23 @@
+import link
+
+llist = link.SLinkedList()
+
+choice = 0
+while choice != 5:
+    choice = input("1: Create list\n2: Add to list\n3: Prepend list\n4: Print List\n5: Exit\n\nSelect One: ")
+    print ("")
+    if choice == 1:
+        val = input("Enter the starting value of the list: ")
+        llist.createHead(val)
+    if choice == 2:
+        val = input("Enter the value you would like to add to the list: ")
+        llist.addVal(val)
+    if choice == 3:
+        val = input("Enter the value you would like to prepend to the list: ")
+        llist.prepend(val)
+    if choice == 4:
+        llist.listprint()
+        print("")
+    if choice == 5:
+        break
+
