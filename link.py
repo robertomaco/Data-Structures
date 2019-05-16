113aad963cd2efda448c53a2ea70f34eb04db8ac
Ryan <ry1nmca@gmail.com>
2019-05-16 02:51:19 +0100

    2 Structures implemented into main

--- link.py
+++ link.py
@@ -1,62 +1,116 @@
-class Node:
-    def __init__(self, dataval=None):
-        self.dataval = dataval
-        self.nextval = None
-        self.tailval = None
+class Node: #initialises node
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
 
 class SLinkedList:
     def __init__(self):
-        self.headval = None
+        self.listHead = None#sets head value
 
-    def listPrint(self):
-        printval = self.headval
+    def createHead(self, value):#creates head node
+        self.listHead = Node(value)
+
+    def listPrint(self):#prints all nodes in a list
+        printval = self.listHead
         while printval is not None:
-            if printval is self.headval:
+            if printval is self.listHead:
                 print "Head"
-            print printval.dataval
-            printval = printval.nextval
+            print printval.data
+            printval = printval.next
 
-    def createHead(self, value):
-        self.headval = Node(value)
-
-    def addVal(self, value):
-        head = self.headval
+    def addVal(self, value): #adds value to the list
+        head = self.listHead
         temp = head
         if head is None:
-            self.createHead(value)
+            self.createHead(value) #creates new head if there is none
         else:
-            while temp.nextval is not None:
-                temp = temp.nextval
-            temp.nextval = Node(value)
+            while temp.next is not None:
+                temp = temp.next
+            temp.next = Node(value) #adds new node to end of list
     
-    def prepend(self, value):
-        head = self.headval
+    def prepend(self, value): #adds a node and sets it as the head 
+        head = self.listHead
         if head is None:
-            self.createHead(value)
+            self.createHead(value) #creates new head if there is none
         else:
-            prependval = Node(value)
-            prependval.nextval = self.headval
-            self.headval = prependval
+            prependval = Node(value) #creates new node
+            prependval.next = self.listHead #sets current head node to the next of the new node
+            self.listHead = prependval #sets new node to head
 
-    def removeNode(self):
-        head = self.headval
+    def removeNode(self): #removes node from list
+        head = self.listHead
         if head is None:
             print "There's no Nodes in the list"
         else:
-            while head.nextval is not None:
+            while head.next is not None:
                 prev = head
-                head = head.nextval
-            prev.nextval = None
+                head = head.next
+            prev.next = None #removes the latest node from list
 
-    def removeByKey(self, key):
-        head = self.headval
-        if head.dataval == key:
-            temp = head.nextval
-            self.headval = temp
+    def removeByKey(self, key): #Looks within list for a node with a given key and removes it
+        head = self.listHead
+        if head.data == key:
+            temp = head.next
+            self.listHead = temp
+            head = None
         else:
-            while head.dataval != key and head.nextval is not None:
+            while head.data != key and head.next is not None:
                 behind = head
-                head = head.nextval
-                if head.nextval is not None:
-                    ahead = head.nextval
-            behind.nextval = ahead
+                head = head.next
+                if head.next is not None:
+                    ahead = head.next
+            behind.next = ahead
+    
+    def insertAfterKey(self, key, value): #Insert value in front of key
+        head = self.listHead
+        newNode = Node(value) #Creates new node
+        if head.data == key:
+            temp = head.next #sets temp = head->next
+            head.next = newNode #sets head->next to new node
+            newNode.next = temp #sets newnode->next to temp head->newnode->temp
+        else:
+            while head.data != key and head is not None:
+                head = head.next
+                if head.data == key:
+                    break
+            if head.next is None:
+                newNode = head.next
+            else:
+                temp = head.next
+                newNode.next = temp
+                head.next = newNode 
+
+    def listLength(self): #lists length of list
+        count = 0
+        head = self.listHead
+        while head is not None:
+            head = head.next
+            count+=1
+        return count
+
+    def sortList(self): #Sorts the list NOT WORKING
+        head = self.listHead
+        val = self.listLength()
+        count = 0
+        print "List length: ", val
+        while head is not None:
+            self.listPrint()
+            if head.next is not None and head == self.listHead and head.next.data < head.data:
+                headtemp = head.next
+                self.listHead = headtemp
+                head.next = headtemp.next
+                headtemp.next = head
+            elif head.next is not None and head != self.listHead and head.next.data < head.data:
+                temp = head.next
+                ahead = temp.next
+                temp.next = head
+                head.next = ahead
+            head = head.next
+
+llist = SLinkedList()
+llist.addVal(5)
+llist.addVal(4)
+llist.addVal(3)
+llist.addVal(2)
+llist.sortList()
+
